4811ebd6a2c618f47c54c96676db3262d582108e
KaungPyaeSoneAung2 <kaungpyae.kpp16@gmail.com>
2023-06-08 00:26:22 +0630

    assignment handover

--- lib/constants/dimes.dart
+++ lib/constants/dimes.dart
@@ -1,7 +1,10 @@
 const double padSmall = 10;
 const double padMid = 20;
 const double padBig = 30;
-const double padLarge = 30;
-const double widthSmall = 10;
-const double widthMid= 10;
-const double widthBig =  10;
+const double padLarge = 40;
+
+const double fontSmall = 18;
+const double fontMid = 25;
+const double fontBig= 35;
+
+
